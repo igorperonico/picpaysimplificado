450b8049830949b9ab2fd78524c4bd2e2d6d8e35
igorperonico <igormtpr@gmail.com>
2024-08-21 13:01:38 -0300

    feat: update UserService

--- src/main/java/com/picpaysimplificado/service/UserService.java
+++ src/main/java/com/picpaysimplificado/service/UserService.java
@@ -2,11 +2,13 @@ package com.picpaysimplificado.service;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -30,6 +32,15 @@ public class UserService {
 
     public void saveUser(User user) {
         this.repository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+    public List<User> getAllUsers(){
+        return repository.findAll();
     }
 
 }
